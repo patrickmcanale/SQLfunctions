b6f41787e59f8c93eb7074adcf554e6f4ae1a482
Pat Canale <73977982+patrickmcanale@users.noreply.github.com>
2021-09-17 13:11:22 -0500

    Add files via upload
    initial commit

---
+++ SQLfunctions.sql
@@ -0,0 +1,86 @@
+-- Create the stored procedure
+CREATE PROCEDURE dbo.cuspImputeTripDistanceMean
+AS
+BEGIN
+-- Specify @AvgTripDistance variable
+DECLARE @AvgTripDistance AS numeric (18,4)
+
+-- Calculate the average trip distance
+SELECT @AvgTripDistance = AVG(TripDistance) 
+FROM YellowTripData
+-- Only include trip distances greater than 0
+WHERE TripDistance > 0
+
+-- Update the records where trip distance is 0
+UPDATE YellowTripData
+SET TripDistance =  @AvgTripDistance
+WHERE TripDistance = 0
+END;
+
+
+
+
+-- Create the function
+CREATE FUNCTION dbo.ConvertMileToKm (@Miles numeric(18,2))
+-- Specify return data type
+RETURNS numeric(18,2)
+AS
+BEGIN
+RETURN
+	-- Convert Miles to Kilometers
+	(SELECT @Miles * 1.609)
+END;
+
+
+
+
+SELECT
+	-- Select the first 100 records of PickupDate
+	TOP 100 PickupDate,
+    -- Determine the shift value of PickupDate
+	dbo.GetShiftNumber(DATEPART(hour, PickupDate)) AS 'Shift',
+    -- Select FareAmount
+	FareAmount,
+    -- Convert FareAmount to Euro
+	dbo.ConvertDollar(FareAmount, 0.87) AS 'FareinEuro',
+    -- Select TripDistance
+	TripDistance,
+    -- Convert TripDistance to kilometers
+	dbo.ConvertMileToKm(TripDistance) AS 'TripDistanceinKM'
+FROM YellowTripData
+-- Only include records for the 2nd shift
+
+
+
+
+CREATE OR ALTER PROCEDURE dbo.cuspBoroughRideStats
+AS
+BEGIN
+SELECT
+    -- Calculate the pickup weekday
+	DATENAME(weekday, PickupDate) AS 'Weekday',
+    -- Select the Borough
+	Zone.Borough AS 'PickupBorough',
+    -- Display AvgFarePerKM as German currency
+	FORMAT(AVG(dbo.ConvertDollar(TotalAmount, .88)/dbo.ConvertMiletoKM(TripDistance)), 'c', 'de-de') AS 'AvgFarePerKM',
+    -- Display RideCount in the German format
+	FORMAT(COUNT (ID), 'n', 'de-de') AS 'RideCount',
+    -- Display TotalRideMin in the German format
+	FORMAT(SUM(DATEDIFF(SECOND, PickupDate, DropOffDate))/60, 'n', 'de-de') AS 'TotalRideMin'
+FROM YellowTripData
+INNER JOIN TaxiZoneLookup AS Zone 
+ON PULocationID = Zone.LocationID
+-- Only include records where TripDistance is greater than 0
+WHERE TripDistance > 0
+-- Group by pickup weekday and Borough
+GROUP BY DATENAME(WEEKDAY, PickupDate), Zone.Borough
+ORDER BY CASE WHEN DATENAME(WEEKDAY, PickupDate) = 'Monday' THEN 1
+	     	  WHEN DATENAME(WEEKDAY, PickupDate) = 'Tuesday' THEN 2
+              WHEN DATENAME(WEEKDAY, PickupDate) = 'Wednesday' THEN 3
+              WHEN DATENAME(WEEKDAY, PickupDate) = 'Thursday' THEN 4
+              WHEN DATENAME(WEEKDAY, PickupDate) = 'Friday' THEN 5
+              WHEN DATENAME(WEEKDAY, PickupDate) = 'Saturday' THEN 6
+              WHEN DATENAME(WEEKDAY, PickupDate) = 'Sunday' THEN 7 END,  
+		 SUM(DATEDIFF(SECOND, PickupDate, DropOffDate))/60
+DESC
+END;
